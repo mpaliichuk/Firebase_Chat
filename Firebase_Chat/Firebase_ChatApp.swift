73a1b1d1e6bab84ad6f52ef2a46b72fd3f52cf49
mpaliichuk <marian.paliichuk@gmail.com>
2024-08-06 14:11:55 +0300

    Initial Commit

---
+++ Firebase_Chat/Firebase_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_ChatApp.swift
+//  Firebase_Chat
+//
+//  Created by Marian Paliichuk on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
